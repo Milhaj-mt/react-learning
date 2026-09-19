13acd5a0c08b070efa573ada51834147dded0372
Milhajmt <milhajmatta08@gmail.com>
2026-09-19 13:40:54 +0530

    Phase 2 Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,69 @@
-function MovieCard(movie){
-    return(
-        <div>
-            <h2>{movie.title}</h2>
-            <p>{movie.rating}</p>
-            <p>{movie.genre}</p>
-        </div>
+import {useState } from "react"
+function RobuverseRental(){
+  
+  return(
+  <>
+    <h1>Robuverse</h1>
+    <p>Future Of Robotics</p>
+  </>
+)}
+function RobotCard({robotName,rentalPrice,robotLocation,children}){
+  const [bookings,setbookings] = useState(0)
+  function increaseBooking(){
+    setbookings(
+      bookings + 1
     )
-}   
+  }
+  function decreaseBooking(){
+    if(bookings > 0){
+      setbookings(
+        bookings - 1
+      )
+    }
+  }
+  return(
+    <>
+    <h2>{robotName}</h2>
+    <p>₹{rentalPrice} / day</p>
+    <p>{robotLocation}</p>
+    {children}
+    <p>Bookings : {bookings}</p>
+    <button onClick={increaseBooking}> . Book</button>
+    <button onClick={decreaseBooking}> . Remove</button>
+    </>
+    
+)}  
 
 function App() {
-    const movies = [
-        {title : "The Mentalist" , rating : "9.9" , genre : "Crime Thriller"},
-        {title : "Breaking Bad" , rating : "9.9" , genre : "Synthetic Thriller"},
-        {title : "ChottaBheem" , rating : "9.9" , genre : "Mass Cartoon Fight"}
-
-    ]
   return (
-    <div>
-      <h1>🎬 Netflix Picks</h1>
-   {
-  movies.map((movie) => {
-    return (
-      <MovieCard title={movie} />
-    )
-  })
-}
-    </div>
-  )
-}
+    <>
+      <RobuverseRental />
 
+      <RobotCard
+        robotName="Stalker"
+        rentalPrice={25000}
+        robotLocation="Kannur"
+      >
+        ⭐ Best for Tech Fests
+      </RobotCard>
+
+      <RobotCard
+        robotName="MJ Replics"
+        rentalPrice={45000}
+        robotLocation="Kollam"
+      >
+        🔥 Most Popular Rental
+      </RobotCard>
+
+      <RobotCard
+        robotName="Optimus Prime"
+        rentalPrice={95000}
+        robotLocation="Malappuram"
+      >
+        ⚡ Premium Experience
+      </RobotCard>
+    </>
+  );
+}
 
 export default App
